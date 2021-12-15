0d793c9de58cecaeadb08e0515995b2dd448036b
K-JM418 <67450735+K-JM418@users.noreply.github.com>
2021-12-15 17:29:52 +0900

    Django 주소록 제작
    기본 틀 완성
    홈페이지는 꾸미기 힘들어서 Pass

--- members_app/views.py
+++ members_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Members
 
-# Create your views here.
+
+def index(request):
+
+    mems = Members.objects.all().order_by('pk')
+
+    return render(
+        request,
+        'members_app/index.html',
+        {
+            'mems': mems
+        }
+    )
+
+def individual_inform(request, pk):
+    mem = Members.objects.get(pk=pk)
+
+    return render(
+        request,
+        'members_app/individual_inform.html',
+        {
+            'mem' : mem,
+        }
+    )
